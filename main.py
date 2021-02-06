0fe30f7597ce4014d524a06a1adaee20a6abb28a
AYehia0 <ahmedyehia611@gmail.com>
2021-02-06 23:03:03 +0200

    main updated with keys handling

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('Init Commit')
+# Importint the Board class
+from board import Board
+
+# To handle key moves aka listener
+import keyboard_moves 
+
+# Creating an object
+b = Board()
+
+
+def main():
+    print(b)
+
+
+
+if __name__ == "__main__":
+    main()
